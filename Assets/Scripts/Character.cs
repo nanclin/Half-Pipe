f0260ecbb89fd5c4a0ab693a94eb98cec92c36e4
Nejc Anclin <nejc.anclin@gmail.com>
2016-07-25 19:21:19 +0200

    Put character on circle.

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+	[SerializeField] private float Angle;
+	[SerializeField] private float Radius;
+
+	void Update () {
+		transform.position = PutOnCircle(Angle, Radius);
+		transform.rotation = Quaternion.Euler(new Vector3(0, 0, Angle));
+	}
+
+	private Vector2 PutOnCircle(float angle, float radius){
+		angle = angle - 90f;
+		Vector2 pos = new Vector2( Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad) ) * radius;
+		return pos;
+	}
+}
