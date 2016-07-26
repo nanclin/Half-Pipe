b0dbe1b920626599804172d0e02b0016d41d78fb
Nejc Anclin <nejc.anclin@gmail.com>
2016-07-26 18:38:32 +0200

    Bezier curve library copied.

---
+++ Assets/BezierExperiment/Scripts/Editor/LineInspector.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/**
+ * Source: http://catlikecoding.com/unity/tutorials/curves-and-splines/
+ */
+
+[CustomEditor(typeof(Line))]
+public class LineInspector : Editor
+{
+
+	// Executes when mouse moves on editor window
+	private void OnSceneGUI()
+	{
+		Line line = target as Line;
+
+		// Take targets transform into account
+		Transform handleTransform = line.transform;
+		// Rotate handles relative to world or transform
+		Quaternion handleRotation = ( Tools.pivotRotation == PivotRotation.Local ) ? handleTransform.rotation : Quaternion.identity;
+		Vector3 p0 = handleTransform.TransformPoint( line.p0 );
+		Vector3 p1 = handleTransform.TransformPoint( line.p1 );
+
+		// Draw line
+		Handles.color = Color.white;
+		Handles.DrawLine( p0, p1 );
+
+		// Add handles
+		Handles.DoPositionHandle( p0, handleRotation );
+		Handles.DoPositionHandle( p1, handleRotation );
+
+		// Assing handle position back to line points.
+		EditorGUI.BeginChangeCheck();
+		p0 = Handles.DoPositionHandle( p0, handleRotation );
+		if( EditorGUI.EndChangeCheck() )
+		{
+			// Before making change, record state
+			// and set state to changed/dirty
+			Undo.RecordObject( line, "Move Point" );
+			EditorUtility.SetDirty( line );
+			// Transform from world space back to lines' local space
+			line.p0 = handleTransform.InverseTransformPoint( p0 );
+		}
+		EditorGUI.BeginChangeCheck();
+		p1 = Handles.DoPositionHandle( p1, handleRotation );
+		if( EditorGUI.EndChangeCheck() ) {
+			// Before making change, record state
+			// and set state to changed/dirty
+			Undo.RecordObject( line, "Move Point" );
+			EditorUtility.SetDirty( line );
+			// Transform from world space back to lines' local space
+			line.p1 = handleTransform.InverseTransformPoint( p1 );
+		}
+	}
+}
---
+++ Assets/BezierExperiment/Scripts/Line.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+/**
+ * Source: http://catlikecoding.com/unity/tutorials/curves-and-splines/
+ */
+
+public class Line : MonoBehaviour
+{
+	public Vector3 p0;
+	public Vector3 p1;
+}
